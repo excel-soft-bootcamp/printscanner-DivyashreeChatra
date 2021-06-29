eee4fcb192729e5e0a574b8d5cec2808569b588c
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-06-29 17:22:33 +0530

    updated

---
+++ PrintScannerProgram/PrintScannerProgram/PrintScanner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintScannerProgram
+{
+    public class PrintScanner
+    {
+        IPrinter printer;
+        IScanner scanner;
+
+        public PrintScanner(Printer _printer)
+        {
+            this.printer = _printer;
+
+        }
+
+        public PrintScanner(Scanner _scanner)
+        {
+            this.scanner = _scanner;
+        }
+
+        public PrintScanner(Printer _printer, Scanner _scanner)
+        {
+            this.printer = _printer;
+            this.scanner = _scanner;
+        }
+
+        public void PrintingOnly()
+        {
+            printer.Print();
+        }
+
+        public void ScanningOnly()
+        {
+            scanner.Scan();
+        }
+
+        public void PrintAndScan()
+        {
+            printer.Print();
+            scanner.Scan();
+        }
+
+
+
+
+    }
+}
---
+++ PrintScannerProgram/PrintScannerProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintScannerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer print = new Printer();
+            print.Print();
+
+            Scanner scan = new Scanner();
+            scan.Scan();
+
+            PrintManager printManager = new PrintManager();
+            printManager.Print(print);
+
+            ScanManager _scanManager = new ScanManager();
+            _scanManager.Scan(scan);
+
+            PrintScanner printonly = new PrintScanner(new PrintManager());
+            printonly.PrintingOnly();
+
+            PrintScanner scanOnly = new PrintScanner(new ScanManager());
+            scanOnly.ScanningOnly();
+
+            PrintScanner printAndScan = new PrintScanner(new PrintManager(), new ScanManager());
+            printAndScan.PrintAndScan();
+
+         }
+    }
+}
